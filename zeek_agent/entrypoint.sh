bd5b29f93cb97971e17f0661deb4c9fda1149be4
Danila <d.tretiakov@innopolis.university>
2024-12-10 19:35:38 +0300

    fix: fix Dockerfile

--- zeek_agent/entrypoint.sh
+++ zeek_agent/entrypoint.sh
@@ -1,5 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
-zeekctl deploy
+if [ -z "$WAZUH_MANAGER_ADDRESS" ] || [ -z "$WAZUH_API_TOKEN" ]; then
+  echo "Не заданы переменные окружения WAZUH_MANAGER_ADDRESS или WAZUH_API_TOKEN."
+  exit 1
+fi
 
-/usr/bin/wazuh-agent -f
+
+/var/ossec/bin/agent-auth -m $WAZUH_MANAGER_ADDRESS -p 55000 -A zeek_agent -P $WAZUH_API_TOKEN
+
+if [ $? -ne 0 ]; then
+  echo "Ошибка при регистрации агента. Проверьте параметры подключения."
+  exit 1
+fi
+
+
+/var/ossec/bin/wazuh-agent -f
