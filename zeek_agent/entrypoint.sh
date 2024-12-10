b7db6e2396c587811f1c3eceac8cf5a787ee511b
Danila <d.tretiakov@innopolis.university>
2024-12-10 19:47:32 +0300

    fix: fix

--- zeek_agent/entrypoint.sh
+++ zeek_agent/entrypoint.sh
@@ -6,7 +6,7 @@ if [ -z "$WAZUH_MANAGER_ADDRESS" ] || [ -z "$WAZUH_API_TOKEN" ]; then
 fi
 
 
-/var/ossec/bin/agent-auth -m $WAZUH_MANAGER_ADDRESS -p 55000 -A zeek_agent -P $WAZUH_API_TOKEN
+/var/ossec/bin/agent-auth -m wazuh.manager -p 55000 -A zeek_agent -P eyJhbGciOiJFUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJ3YXp1aCIsImF1ZCI6IldhenVoIEFQSSBSRVNUIiwibmJmIjoxNzMzODQ5MDQ0LCJleHAiOjE3MzM4NDk5NDQsInN1YiI6IndhenVoLXd1aSIsInJ1bl9hcyI6ZmFsc2UsInJiYWNfcm9sZXMiOlsxXSwicmJhY19tb2RlIjoid2hpdGUifQ.Ad7EJTZJk77yFkqL6TEOh4Me1toACFQ9uRNUfoohTtgpNAKO-ebvv_VlcoRuKIAXhywBkzGjuWbAi4f9b2cz3WTpAWTdeVgcHhQjDvkqNhVughcRP7Hn9dVPIEjEpnuT9rBBSBuElyUmhLIS7Bw3qm-peNPZaT9nF8qv4jODVa6g6ozx
 
 if [ $? -ne 0 ]; then
   echo "Ошибка при регистрации агента. Проверьте параметры подключения."
